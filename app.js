050a990cfe68977311c3809f2925fbf00abb8289
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-10-20 21:20:37 +0300

    Adding Mongoose

--- app.js
+++ app.js
@@ -1,46 +1,70 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+const express    = require("express"),
+      app        = express(),
+      bodyParser = require("body-parser"),
+      mongoose   = require('mongoose'),
+      PORT       = 3000;
 
-const PORT = 3000;
-
+// CONFIG
+mongoose.connect('mongodb://localhost:27017/Yelpcamp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
+// SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Yosemite Valley",
+//     image: "https://images.unsplash.com/photo-1465695954255-a262b0f57b40?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=06d92f647a2937af54f658e199c3d990&auto=format&fit=crop&w=1050&q=80"
+// }, function(error, campground) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log("NEWLY CREATED CAMPGROUND: ");
+//         console.log(campground);
+//     }
+// });
     
-app.get("/", function(req, res){
-    res. render("landing");
+app.get("/", (req, res) => {
+    res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+//GET ALL : API/CAMPGROUNDS
+app.get("/campgrounds", (req, res) => {
+    Campground.find({}, (err, allCampground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res. render("campgrounds", {campgrounds : allCampground});
+        }
+    })
 });
 
-app.post("/campgrounds", function(req, res){
-    // get data from form and add to campgrounds array
+//POST : API/CAMPGROUNDS
+app.post("/campgrounds", (req, res) => {
+    // get data from form and add to campgrounds db
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    var newCampground = {name: name, image: image};
+
+    Campground.create( newCampground, err => {
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
+    
 });
 
-app.get("/campgrounds/new", function(req, res){
+app.get("/campgrounds/new", (req, res) => {
    res.render("new.ejs"); 
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, () => {
    console.log("The YelpCamp Server Has Started On Port 3000!");
 });
