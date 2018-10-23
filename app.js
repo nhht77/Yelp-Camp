7dcc3285cd5c0865f66a19ab945aaedd4f7bef38
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-10-23 23:26:41 +0300

    Fixing modals btn submit error

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const express    = require("express"),
 mongoose.connect('mongodb://localhost:27017/Yelpcamp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
     
 app.get("/", (req, res) => {
