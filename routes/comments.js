0c08deb84c255895a2db440ed761862e40541d6a
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-11-03 00:39:40 +0200

    Comment Edit

--- routes/comments.js
+++ routes/comments.js
@@ -28,6 +28,29 @@ router.post("/", isLoggedIn ,(req, res) => {
         });
     });
 
+// COMMENT UPDATE
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+       if(err){
+           res.redirect("back");
+       } else {
+           res.redirect("/campgrounds/" + req.params.id );
+       }
+    });
+ });
+ 
+ // COMMENT DESTROY ROUTE
+ router.delete("/:comment_id", (req, res) => {
+     //findByIdAndRemove
+     Comment.findByIdAndRemove(req.params.comment_id, err => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+     });
+ });
+
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()){
         return next();
